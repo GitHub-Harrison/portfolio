9c2854a7e866ae3b5ff6b7a3e80de564c16ba30d
GitHub-Harrison <harrison-johnson@outlook.com>
2022-11-28 19:10:36 +0000

    Rename views to index to match index.html

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
+def index(request):
     """ a view to return the home page """
 
     return render(request, 'home/index.html')
