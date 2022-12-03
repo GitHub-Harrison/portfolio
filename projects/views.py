69e29bd72430b20518e9ba8cafb903400ca17099
GitHub-Harrison <harrison-johnson@outlook.com>
2022-12-03 15:34:05 +0000

    Create view to show projects app webpage

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def projects(request):
+    """ a view to return the home page """
+
+    return render(request, 'projects/projects.html')
